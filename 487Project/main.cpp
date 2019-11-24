e9aa79b41eafbaa9ff1f13a6a246d5b130858e05
Sean Miles <sean.miles@outlook.com>
2019-11-24 14:16:21 -0800

    Initial commit

---
+++ 487Project/main.cpp
@@ -0,0 +1,17 @@
+// main.cpp
+// Purpose
+// Author: Sean Miles, Bradley Pratt, Tung Dinh
+// CSS 487 - Fall Quarter 2019
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace cv;
+
+// main
+// precondition: A
+// postconditions: A
+int main(int argc, char* argv[])
+{
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CSS487_Project
+ 
