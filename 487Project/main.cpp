3ad1e1e8faa945db7c322f96d62b251175d7c6d6
tungxd96 <tungxd301@gmail.com>
2019-11-26 20:45:53 -0800

    Able to get center of contours

--- 487Project/main.cpp
+++ 487Project/main.cpp
@@ -32,16 +32,29 @@ int main(int argc, char* argv[])
 	// Erosion
 	erode(edged, edged, NULL);
 
-	vector<vector<Point> > contours;
+	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
 	findContours(edged, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
+	// Get the moments
+	vector<Moments> mu(contours.size());
+	for (int i = 0; i < contours.size(); i++) {
+		mu[i] = moments(contours[i], false);
+	}
+	// Get the mass centers
+	vector<Point2f> mc(contours.size());
+	for (int i = 0; i < contours.size(); i++) {
+		mc[i] = Point2f(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+	}
+
+	// Draw contours
 	Mat drawing = Mat::zeros(edged.size(), CV_8UC3);
 	for (int i = 0; i < contours.size(); i++) {
 		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
 		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+		circle(drawing, mc[i], 4, color, -1, 8, 0);
 	}
-
+	cout << "Center of contours = " << mc[0] << endl;
 	// Find contours in edge map
 	namedWindow("Picture");
 	imshow("Picture", drawing);
